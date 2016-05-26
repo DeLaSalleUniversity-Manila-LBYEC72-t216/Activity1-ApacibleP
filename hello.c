5cc11fa22a5d8505c34e248b343e5f8fb9668ce8
patrickapacible <patrick_apacible@dlsu.edu.ph>
2016-05-26 16:51:24 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,22 @@
+```
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+	printf("\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+	printf("\t\tPatrick Anthony M. Apacible\n");
+	printf("\t\t\tBS CIV\n");
+	printf("\t\t\t114\n");
+	printf("\t\t\tMay 26, 2016\n");
+	printf("\t\t\tfirst_last@dlsu.edu.ph\n");
+	printf("\t\t\t01234567890\n\n");
+	printf("\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+	system("PAUSE");
+	return 0;
+		
+}
+```
+![](hello.png)
